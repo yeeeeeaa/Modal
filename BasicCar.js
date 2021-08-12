00730c3718a8a5b2f1822cf699661925fe081232
김민송 <tlsdks03@naver.com>
2021-08-12 23:30:00 +0900

    plus emotion, shared link UI

---
+++ BasicCar.js
@@ -0,0 +1,16 @@
+class BasicCar{
+constructor(color, maxSpeed,price){
+    this.color=color;
+    this.maxSpeed=maxSpeed;
+    this.price=price;
+    }
+
+    run() {
+        console.log('Running...');
+    }
+
+
+    stop() {
+        console.log('Stop!');
+    }
+}
